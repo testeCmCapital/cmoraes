64fe0597d2436c370d16f6a9b87a83ff1a22355d
cristiano.moraes <cristiano.moraes@ituran.com.br>
2023-05-21 05:07:10 -0300

    Ajuste de propriedades nullable do retorno

--- CmCapitalSalesTest/Domain/DTOs/JsonReturn.cs
+++ CmCapitalSalesTest/Domain/DTOs/JsonReturn.cs
@@ -2,8 +2,8 @@
 {
     public class JsonReturn
     {
-        public object Data { get; set; }
+        public object? Data { get; set; }
         public bool IsSucess { get; set; }
-        public List<Message> Messages { get; set; }
+        public List<Message>? Messages { get; set; }
     }
 }
